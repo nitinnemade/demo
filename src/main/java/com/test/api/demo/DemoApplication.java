bf8600d36e4988e6462a8551cb898584b4ee508c
nitinnemade <49411366+nitinnemade@users.noreply.github.com>
2021-06-07 00:50:57 +0530

    Update DemoApplication.java
    updated  again

--- src/main/java/com/test/api/demo/DemoApplication.java
+++ src/main/java/com/test/api/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Branch changes");
+		System.out.println("Master changes");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
