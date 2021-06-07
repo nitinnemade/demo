9182d056cc9876089e608ee5200cf9849875a09a
nitinnemade <49411366+nitinnemade@users.noreply.github.com>
2021-06-07 08:23:33 +0530

    Update DemoApplication.java

--- src/main/java/com/test/api/demo/DemoApplication.java
+++ src/main/java/com/test/api/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("BIN changes");
+		System.out.println("BIN changes");
+		System.out.println("BIN changes");
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("dfss changes");
 	}
