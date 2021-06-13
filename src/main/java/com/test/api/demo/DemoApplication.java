681962fcfe916755f1dc64c750c1ad749b03d3d0
Nitin_n16 <nemade.nitin@gmail.com>
2021-06-13 17:44:07 +0530

    test commit

--- src/main/java/com/test/api/demo/DemoApplication.java
+++ src/main/java/com/test/api/demo/DemoApplication.java
@@ -8,13 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Three bin changes");
-		System.out.println("PBIN changes");
-		System.out.println("BIN changes");
-		System.out.println("BIN changes");
-		System.out.println("BIN changes");
+		
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("dfss changes");
+		
 	}
 
 }
---
+++ src/main/java/com/test/api/demo/Test.java
@@ -0,0 +1,9 @@
+package com.test.api.demo;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Test branch");
+	}
+
+}
